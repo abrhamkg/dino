e1ed19bd214cd43c4e0087948d8a5154af560408
Abrham Kahsay Gebreselasie <kgabrham@gmail.com>
2025-04-26 17:57:24 +0400

    Dino initial: second

---
+++ sbatch_train.sh
@@ -0,0 +1,21 @@
+#SBATCH --ntasks=1
+#SBATCH --mem=32GB
+#SBATCH --time=00:10:00
+#SBATCH --job-name=dino_s_info_drop_subset
+#SBATCH --output=slurm_logs/dino_s_info_drop_subset.out
+#SBATCH --error=slurm_logs/dino_s_info_drop_subset.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=kgabrham@gmail.com
+#SBATCH --partition=cscc-gpu-p
+#SBATCH -q cscc-gpu-qos
+
+python -u train_dino_nowds.py   --arch "resnext50_32x4d" \
+        --patch_size 14 \
+        --num_workers 8 \
+        --lr 0.0001 \
+        --min_lr 0.0001 \
+        --optimizer adamw\
+        --output_dir . \
+        --data_path /l/users/abrham.gebreselasie/datasets/SAYCam/imfldr_train_5fps/  \
+        --save_prefix "dino_s_info_drop_subset" \
+        --batch_size 32
